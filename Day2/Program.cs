eb55eb52db1a77643f022ac3c3d3e33e6ee60bfe
Jan Broeze <jan.broeze@sanoma.com>
2021-12-22 13:47:06 +0100

    Day 2

---
+++ Day2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+List<(string, int)> Load(string inputPath)
+{
+    var fs = new FileStream(inputPath, FileMode.Open, FileAccess.Read);
+    var list = new List<(string, int)>();
+    using(var sr = new StreamReader(fs, Encoding.UTF8))
+    {
+        string l;
+        while((l = sr.ReadLine()) != null)
+        {
+            var dir = l.Split(" ");
+            int.TryParse(dir[1], out var i);
+            list.Add((dir[0], i));
+        }
+    }
+
+    return list;
+}
+
+void SolveA(string inputPath)
+{
+    var items = Load(inputPath);
+    int x = 0, y1 = 0, y2 = 0, a = 0;
+    foreach(var i in items)
+    {
+        switch (i.Item1)
+        {
+            case "forward": 
+                x += i.Item2;
+                y2 += i.Item2 * a;
+                break;
+            case "down":
+                y1 -= i.Item2;
+                a += i.Item2;
+                break;
+            case "up":
+                y1 += i.Item2;
+                a -= i.Item2;
+                break;
+            default:
+                throw new Exception();
+        }
+    }
+    
+    Console.WriteLine($"Part A: {Math.Abs(x) * Math.Abs(y1)}");
+    Console.WriteLine($"Part A: {Math.Abs(x) * Math.Abs(y2)}");
+}
+
+SolveA("ex.txt");
+SolveA("input.txt");
